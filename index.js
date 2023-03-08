765c9f7b25843c7ad269c19bff2019a616dcc7d3
fransi-kurti <fransi.kurti@gmail.com>
2023-03-08 23:11:04 +0100

    finished javascript part

--- index.js
+++ index.js
@@ -7,12 +7,18 @@ const secondInput = document.getElementById("second-input");
 
 const exchangeRate = document.getElementById("exchange-rate");
 
+calculateInput();
 
 function calculateInput(){
     fetch(`https://v6.exchangerate-api.com/v6/197f4d36c6c6d56b6a664280/latest/${firstCurrency.value}`
     )
     .then((res) => res.json())
-    .then((data) => console.log(data))
+    .then((data) => {
+        const rate = data.conversion_rates[secondCurrency.value];
+        console.log(rate);
+        exchangeRate.innerText = `1 ${firstCurrency.value} = ${rate + " "+secondCurrency.value}`
+        secondInput.value = (firstInput.value * rate).toFixed(3);
+});
 
     
 }
